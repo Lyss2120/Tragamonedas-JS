708e39fbfd455e061d3a72b52e362c4391dfcf0f
Lyss2120 <lys.ivonne@gmail.com>
2026-05-08 19:20:30 -0400

    deposit, lines, bet functions

--- project.js
+++ project.js
@@ -8,6 +8,23 @@
 
 
 const prompt = require('prompt-sync')(); //require the package imports into the program, and the second parentesis gives acces to the function that you can use to set the user input
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT =
+{
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8
+}
+const SYMBOL_VALUE = {
+    A: 5,
+    B: 4,
+    C: 3,
+    D: 2
+}
+
 
 const deposit = () => {
     // loop forever till the else condition is met, that break the loop    
@@ -22,7 +39,6 @@ const deposit = () => {
         }
     }
 }
-
 
 const getNumberLines = () => {
 
@@ -41,15 +57,46 @@ const getNumberLines = () => {
 
 const getBet = (balance, lines) => {
     while (true) {
-        const bet = prompt('Enter the total bet: ');
+        const bet = prompt('Enter the bet per line: ');
+        const numberBet = parseFloat(bet)
 
-        if (isNaN(numberBet) || numberBet >= 0 || numberBet > balance / lines)
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {// numberBet > balance / lines: revisa si el monto de las apuestas (bet per line) no es mayor al $ total / por el nro de apuestas
             console.log('Invalid bet, try again');
-            
-    } // numberBet > balance / lines: revisa si el monto de cada apuesta es menor al $ total/por las lineas a jugar o no alcanza
+        } else { return numberBet }
+    }
+}
+const spin = () => {
+    const symbols = [];
 
-let balance = deposit();
-const numberOfLines = getNumberLines();
-const bet = getBet(balance, numberOfLines);
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol); //envia el numero de simbolos correspondiente al array symbols 2 A, 4 B, 6 C, 8 D
+        }
+    }    
+    const reels = [[], [], []]; //crea 3 espacios para los simbolos
+    
+    for (let i = 0; i < COLS; i++) {
+        const reelSymbols = [...symbols]//copia el array symbols para tener disponibles los simbolos en cada linea
+        for (let j = 0; j < ROWS; j++) {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length)
+            const selectedSymbol = reelSymbols[randomIndex]//selecciona un simbolo al azar
+            reels[i].push(selectedSymbol)//añade el simbolo su posicion en la linea
+            reelSymbols.splice(randomIndex, 1) //elimina el elemento del array reelSymbols
+            // console.log({j}, {randomIndex}, {selectedSymbol}, {reelSymbols}, {reels});
+        }
+    }
+    return reels;
+     
+}
+console.log(spin());
+
+spin()
+
+
+
+// let balance1 = deposit();
+// const numberOfLines = getNumberLines();
+// const bet = getBet(balance1, numberOfLines);
+
 
 
